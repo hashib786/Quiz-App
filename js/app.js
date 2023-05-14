713bd394a4ef7b5da8bd025b8ba20e06dda608a5
Hashib <hashibraja23@gmail.com>
2023-05-14 20:58:05 +0530

    added Draging fanctionality when option is not selcted and i want to go on next question

--- js/app.js
+++ js/app.js
@@ -20,152 +20,152 @@ const questions = [
     answers: ["size", "fontsize", "font", "font-size"],
     correctAnswer: "font-size",
   },
-  {
-    id: 3,
-    question: "What is the JavaScript function for creating a new element?",
-    answers: [
-      "createElement",
-      "newElement",
-      "document.createElement",
-      "createElement",
-    ],
-    correctAnswer: "document.createElement",
-  },
-  {
-    id: 4,
-    question: "What is the HTML tag for an image?",
-    answers: ["photo", "picture", "image", "img"],
-    correctAnswer: "img",
-  },
-  {
-    id: 5,
-    question: "What is the CSS property for setting the background color?",
-    answers: ["bg", "color", "bgcolor", "background-color"],
-    correctAnswer: "background-color",
-  },
-  {
-    id: 6,
-    question: "What is the JavaScript function for adding an event listener?",
-    answers: ["addEvent", "on", "attachEvent", "addEventListener"],
-    correctAnswer: "addEventListener",
-  },
-  {
-    id: 7,
-    question: "What is the HTML tag for a paragraph?",
-    answers: ["text", "para", "paragraph", "p"],
-    correctAnswer: "p",
-  },
-  {
-    id: 8,
-    question: "What is the CSS property for setting the text color?",
-    answers: ["textColor", "tcolor", "color", "text-color"],
-    correctAnswer: "color",
-  },
-  {
-    id: 9,
-    question:
-      "What is the JavaScript function for getting the value of an input element?",
-    answers: ["getValue", "getInput", "input", "value"],
-    correctAnswer: "value",
-  },
-  {
-    id: 10,
-    question: "What is the HTML tag for a link?",
-    answers: ["href", "link", "url", "a"],
-    correctAnswer: "a",
-  },
-  {
-    id: 11,
-    question: "What is the CSS property for setting the link color?",
-    answers: ["linkColor", "lcolor", "color", "link-color"],
-    correctAnswer: "color",
-  },
-  {
-    id: 12,
-    question: "What is the JavaScript function for navigating to a new page?",
-    answers: ["navigate", "window.location", "location.href", "go"],
-    correctAnswer: "location.href",
-  },
-  {
-    id: 13,
-    question: "What is the HTML tag for a form?",
-    answers: ["submit", "button", "input", "form"],
-    correctAnswer: "form",
-  },
-  {
-    id: 14,
-    question: "What is the CSS property for setting the border width?",
-    answers: ["width", "bw", "border", "border-width"],
-    correctAnswer: "border-width",
-  },
-  {
-    id: 15,
-    question: "What is the JavaScript function for submitting a form?",
-    answers: ["go", "send", "post", "submit"],
-    correctAnswer: "submit",
-  },
-  {
-    id: 16,
-    question: "What is the HTML tag for a table?",
-    answers: ["table", "tbl", "grid", "data"],
-    correctAnswer: "table",
-  },
-  {
-    id: 17,
-    question: "What is the CSS property for setting the table width?",
-    answers: ["table-width", "width", "w", "tw"],
-    correctAnswer: "width",
-  },
-  {
-    id: 18,
-    question: "What is the JavaScript function for adding a row to a table?",
-    answers: ["newRow", "addRow", "row", "insertRow"],
-    correctAnswer: "insertRow",
-  },
-  {
-    id: 19,
-    question: "What is the HTML tag for a cell?",
-    answers: ["cell", "data", "text", "td"],
-    correctAnswer: "td",
-  },
-  {
-    id: 20,
-    question: "What is the CSS property for setting the cell width?",
-    answers: ["cw", "w", "cell-width", "width"],
-    correctAnswer: "width",
-  },
-  {
-    id: 21,
-    question:
-      "What is the JavaScript function for getting the value of a cell?",
-    answers: ["getCell", "getValue", "getCellValue", "value"],
-    correctAnswer: "value",
-  },
-  {
-    id: 22,
-    question: "What is the HTML tag for a button?",
-    answers: ["btn", "press", "button", "click"],
-    correctAnswer: "button",
-  },
-  {
-    id: 23,
-    question: "What is the CSS property for setting the button width?",
-    answers: ["w", "bw", "button-width", "width"],
-    correctAnswer: "width",
-  },
-  {
-    id: 24,
-    question:
-      "What is the JavaScript function for adding an event listener to a button?",
-    answers: ["on", "attachEvent", "addEvent", "addEventListener"],
-    correctAnswer: "addEventListener",
-  },
-  {
-    id: 25,
-    question: "What is the HTML tag for a form input?",
-    answers: ["form-input", "fi", "input", "i"],
-    correctAnswer: "input",
-  },
+  // {
+  //   id: 3,
+  //   question: "What is the JavaScript function for creating a new element?",
+  //   answers: [
+  //     "createElement",
+  //     "newElement",
+  //     "document.createElement",
+  //     "createElement",
+  //   ],
+  //   correctAnswer: "document.createElement",
+  // },
+  // {
+  //   id: 4,
+  //   question: "What is the HTML tag for an image?",
+  //   answers: ["photo", "picture", "image", "img"],
+  //   correctAnswer: "img",
+  // },
+  // {
+  //   id: 5,
+  //   question: "What is the CSS property for setting the background color?",
+  //   answers: ["bg", "color", "bgcolor", "background-color"],
+  //   correctAnswer: "background-color",
+  // },
+  // {
+  //   id: 6,
+  //   question: "What is the JavaScript function for adding an event listener?",
+  //   answers: ["addEvent", "on", "attachEvent", "addEventListener"],
+  //   correctAnswer: "addEventListener",
+  // },
+  // {
+  //   id: 7,
+  //   question: "What is the HTML tag for a paragraph?",
+  //   answers: ["text", "para", "paragraph", "p"],
+  //   correctAnswer: "p",
+  // },
+  // {
+  //   id: 8,
+  //   question: "What is the CSS property for setting the text color?",
+  //   answers: ["textColor", "tcolor", "color", "text-color"],
+  //   correctAnswer: "color",
+  // },
+  // {
+  //   id: 9,
+  //   question:
+  //     "What is the JavaScript function for getting the value of an input element?",
+  //   answers: ["getValue", "getInput", "input", "value"],
+  //   correctAnswer: "value",
+  // },
+  // {
+  //   id: 10,
+  //   question: "What is the HTML tag for a link?",
+  //   answers: ["href", "link", "url", "a"],
+  //   correctAnswer: "a",
+  // },
+  // {
+  //   id: 11,
+  //   question: "What is the CSS property for setting the link color?",
+  //   answers: ["linkColor", "lcolor", "color", "link-color"],
+  //   correctAnswer: "color",
+  // },
+  // {
+  //   id: 12,
+  //   question: "What is the JavaScript function for navigating to a new page?",
+  //   answers: ["navigate", "window.location", "location.href", "go"],
+  //   correctAnswer: "location.href",
+  // },
+  // {
+  //   id: 13,
+  //   question: "What is the HTML tag for a form?",
+  //   answers: ["submit", "button", "input", "form"],
+  //   correctAnswer: "form",
+  // },
+  // {
+  //   id: 14,
+  //   question: "What is the CSS property for setting the border width?",
+  //   answers: ["width", "bw", "border", "border-width"],
+  //   correctAnswer: "border-width",
+  // },
+  // {
+  //   id: 15,
+  //   question: "What is the JavaScript function for submitting a form?",
+  //   answers: ["go", "send", "post", "submit"],
+  //   correctAnswer: "submit",
+  // },
+  // {
+  //   id: 16,
+  //   question: "What is the HTML tag for a table?",
+  //   answers: ["table", "tbl", "grid", "data"],
+  //   correctAnswer: "table",
+  // },
+  // {
+  //   id: 17,
+  //   question: "What is the CSS property for setting the table width?",
+  //   answers: ["table-width", "width", "w", "tw"],
+  //   correctAnswer: "width",
+  // },
+  // {
+  //   id: 18,
+  //   question: "What is the JavaScript function for adding a row to a table?",
+  //   answers: ["newRow", "addRow", "row", "insertRow"],
+  //   correctAnswer: "insertRow",
+  // },
+  // {
+  //   id: 19,
+  //   question: "What is the HTML tag for a cell?",
+  //   answers: ["cell", "data", "text", "td"],
+  //   correctAnswer: "td",
+  // },
+  // {
+  //   id: 20,
+  //   question: "What is the CSS property for setting the cell width?",
+  //   answers: ["cw", "w", "cell-width", "width"],
+  //   correctAnswer: "width",
+  // },
+  // {
+  //   id: 21,
+  //   question:
+  //     "What is the JavaScript function for getting the value of a cell?",
+  //   answers: ["getCell", "getValue", "getCellValue", "value"],
+  //   correctAnswer: "value",
+  // },
+  // {
+  //   id: 22,
+  //   question: "What is the HTML tag for a button?",
+  //   answers: ["btn", "press", "button", "click"],
+  //   correctAnswer: "button",
+  // },
+  // {
+  //   id: 23,
+  //   question: "What is the CSS property for setting the button width?",
+  //   answers: ["w", "bw", "button-width", "width"],
+  //   correctAnswer: "width",
+  // },
+  // {
+  //   id: 24,
+  //   question:
+  //     "What is the JavaScript function for adding an event listener to a button?",
+  //   answers: ["on", "attachEvent", "addEvent", "addEventListener"],
+  //   correctAnswer: "addEventListener",
+  // },
+  // {
+  //   id: 25,
+  //   question: "What is the HTML tag for a form input?",
+  //   answers: ["form-input", "fi", "input", "i"],
+  //   correctAnswer: "input",
+  // },
 ];
 
 // ***************************
@@ -461,7 +461,12 @@ const questionStart = (targetQuiz) => {
   // Add Event Listner for next Button To Visible new Quesion
   const nextButton = targetQuiz.querySelector(".question__next p");
   nextButton.addEventListener("click", () => {
-    if (isWrite === undefined && timing !== 0) return;
+    if (isWrite === undefined && timing !== 0) {
+      const answerContainer = targetQuiz.querySelector(".answer__container");
+      answerContainer.classList.add("drag");
+      setTimeout(() => answerContainer.classList.remove("drag"), 300);
+      return;
+    }
     nextQuestion(targetQuiz);
   });
 };
